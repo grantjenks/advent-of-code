1a5d6e34ab310939db60a16ed03b6440f8cffdff
Grant Jenks <gjenks@linkedin.com>
2021-12-07 22:59:37 -0800

    Solution for day 8

---
+++ 2021/day8.py
@@ -0,0 +1,37 @@
+# Part 1
+lines = text.splitlines()
+lines = [line.split(' | ') for line in lines]
+lines = [[left.split(), right.split()] for left, right in lines]
+sum(1 for _, right in lines for token in right if len(token) in {2, 4, 3, 7})
+
+# Part 2
+def codings(nums):
+    nums = list(map(frozenset, nums))
+    one = next(num for num in nums if len(num) == 2)
+    four = next(num for num in nums if len(num) == 4)
+    seven = next(num for num in nums if len(num) == 3)
+    three = next(num for num in nums if len(num) == 5 and one < num)
+    six = next(num for num in nums if len(num) == 6 and not (one < num))
+    nine = next(num for num in nums if len(num) == 6 and one < num and four < num)
+    zero = next(num for num in nums if len(num) == 6 and num != six and num != nine)
+    eight = next(num for num in nums if len(num) == 7)
+    five = next(num for num in nums if len(num) == 5 and (four - one) < num)
+    two = next(iter(set(nums) - {zero, one, three, four, five, six, seven, eight, nine}))
+    return {
+        zero: 0,
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9,
+    }
+
+def parse(left, right):
+    codes = codings(left)
+    return int(''.join(map(str, list(map(codes.get, map(frozenset, right))))))
+
+sum(parse(left, right) for left, right in lines)
