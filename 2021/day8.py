f51ab973ca494672d858cf1ca53bb0308e476d5b
Grant Jenks <gjenks@linkedin.com>
2021-12-07 23:06:12 -0800

    Cleanup day 8 a little

--- 2021/day8.py
+++ 2021/day8.py
@@ -10,25 +10,14 @@ def codings(nums):
     one = next(num for num in nums if len(num) == 2)
     four = next(num for num in nums if len(num) == 4)
     seven = next(num for num in nums if len(num) == 3)
+    eight = next(num for num in nums if len(num) == 7)
     three = next(num for num in nums if len(num) == 5 and one < num)
+    five = next(num for num in nums if len(num) == 5 and (four - one) < num)
     six = next(num for num in nums if len(num) == 6 and not (one < num))
     nine = next(num for num in nums if len(num) == 6 and one < num and four < num)
     zero = next(num for num in nums if len(num) == 6 and num != six and num != nine)
-    eight = next(num for num in nums if len(num) == 7)
-    five = next(num for num in nums if len(num) == 5 and (four - one) < num)
     two = next(iter(set(nums) - {zero, one, three, four, five, six, seven, eight, nine}))
-    return {
-        zero: 0,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-    }
+    return {zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9}
 
 def parse(left, right):
     codes = codings(left)
